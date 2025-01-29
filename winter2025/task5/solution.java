0196de95db12dd2338d1e3901917f63783cf8991
Juli <62210049+xoxoginger@users.noreply.github.com>
2025-01-30 00:06:49 +0300

    fix solution
    Status: OK

--- winter2025/task5/solution.java
+++ winter2025/task5/solution.java
@@ -37,3 +37,60 @@ f(1,1)+f(1,2)+f(1,3)+f(2,2)+f(2,3)+f(3,3) = 1+1+2+1+2+1 = 8
 Вывод
 8
 */
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        long s = scanner.nextLong();
+        long[] a = new long[n + 1];
+        long[] S = new long[n + 2];
+        
+        for (int i = 1; i <= n; i++)
+            a[i] = scanner.nextLong();
+
+        for (int i = 1; i <= n; i++)
+            S[i] = S[i - 1] + a[i];
+
+        long totalCuts = 0;
+        for (int i = 1; i <= n - 1; i++) {
+            long currentSum = S[i];
+            long A = currentSum - s;
+            int highK = i - 1;
+            int k = binarySearch(S, highK, A);
+
+            if (k == -1)
+                continue;
+
+            int x = k + 1;
+            long sumWithNext = S[i + 1] - S[k];
+            
+            if (sumWithNext > s) {
+                long contribution = (long) x * (n - i);
+                totalCuts += contribution;
+            }
+        }
+
+        long totalSubarrays = (long) n * (n + 1) / 2;
+        System.out.println(totalCuts + totalSubarrays);
+    }
+    
+    public static int binarySearch(long[] S, int high, long A) {
+        int low = 0;
+        int res = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (S[mid] >= A) {
+                res = mid;
+                low = mid + 1;
+            } 
+            else
+                high = mid - 1;
+        }
+        return res;
+    }
+}
